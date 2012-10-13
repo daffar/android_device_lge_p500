bc0da8d7c2beb86460e2066ebebdf4a824c244f0
Mihai Costea <mihai.andrei.costea@gmail.com>
2012-10-13 15:36:37 +0300

    Revert "Remove prebuilt OTA Updater"
    This reverts commit 8ebbacde2344c42498bce1ebac6eac7c09c92150.

--- device.mk
+++ device.mk
@@ -40,6 +40,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nvram.txt:system/etc/wl/nvram.txt \
     $(LOCAL_PATH)/configs/vold.fstab:system/etc/vold.fstab \
     $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+    
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/prebuilt/OTAUpdater.apk:system/app/OTAUpdater.apk
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.camera=msm7x27 \
